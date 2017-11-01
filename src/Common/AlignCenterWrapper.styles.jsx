7e50bcdff66944e226b52570feef2151029b1c07
laurage <laurage_@hotmail.com>
2017-11-01 17:47:57 +0000

    removed files that were moved from one folder to another

--- src/Common/AlignCenterWrapper.styles.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const AlignCenterWrapper = styled.div`
-  text-align: center;
-`
--- src/Common/BannerWrapper.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const BannerWrapper = styled.div`
-  background-color: ${props => props.theme.colors.primaryDarker};
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
