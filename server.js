19c3a20b4ef2a20e38036e4c125b88e56c6ae00a
laurage <laurage_@hotmail.com>
2017-11-07 10:43:07 +0000

    trying to connect to firebase

--- server.js
+++ server.js
@@ -4,14 +4,25 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+var firebase = require('firebase');
+
+
 // Answer API requests.
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/players', function (req, res) {
-  res.json({players: [{playerId: 1, playerName: "Laura"}, {playerId: 2, playerName: "Anne"}, {playerId: 3, playerName: "Mark"}]});
+    firebase.database().ref('/').set({
+      username: "test",
+      email: "test@mail.com"
+  });
+
+
+  // res.json({players: [{playerId: 1, playerName: "Laura"}, {playerId: 2, playerName: "Anne"}, {playerId: 3, playerName: "Mark"}]});
 });
+
+
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
