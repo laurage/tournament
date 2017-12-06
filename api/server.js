78d447b4825a3bf456f1d712dda1bb23324a7d4d
Laura <laurage_@hotmail.com>
2017-12-06 15:38:29 +0000

    only one dockerfile now used (#15)

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
